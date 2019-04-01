1b2a1cc8cd4a22f736e1142d379e94ee1ba897ac
RaxxCoin <raxxcoin@gmail.com>
2019-03-31 22:55:15 -0700

    add bank

--- Assets/PlayerCreationFactory.sol
+++ Assets/PlayerCreationFactory.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.5.0;
 
 Contract PlayerCreationFactory {
+
+mapping(address => address []) created;
+
+function createPlayer(uint256 _initialAmount) public returns (address) {
+  Player newPlayer = newPlayer(_initialAmount);
+  crated[msg.sender].push(address(newPlayer));
+  newPlayer.transfer(msg.sender, _initialAmount);
+  return address(newPlayer);
+}
+
+
   Struct Player {
     string name;
     uint badge;
